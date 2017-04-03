4f2037f534c8203528b1e51d99019cfffc99ad28
Robert Starmer <rstarmer@gmail.com>
2017-04-02 22:31:53 -0400

    Update for Kubeadm 1.5.6 and RBAC changes

--- kubeadm-master-ubuntu.sh
+++ kubeadm-master-ubuntu.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
-set -x
+
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-get install curl \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo apt-key adv \
-#               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-#               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-#echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
+echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
 echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-#sudo apt-get install docker-engine=1.12.6-0~ubuntu-xenial -y
-sudo apt-get install docker-engine -y
-sudo apt-get install kubeadm kubectl kubernetes-cni -y
+sudo apt-get install docker-engine=1.12* -y
 sudo service docker start
 sudo groupadd docker
-sudo usermod -aG docker ubuntu 
+sudo usermod -aG docker ubuntu
 sudo apt-get install python-pip -y
 sudo apt-get install joe -y
 sudo -H pip install --upgrade pip
 
 #Setup Kubernetes
-
+sudo apt-get install kubeadm=1.5.6* kubelet=1.5.6* kubectl=1.5.6* kubernetes-cni=0.5.1* -y
 ADDRESS="$(ip -4 addr show enp0s8 | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
 sudo sed -e "s/^.*master.*/${ADDRESS} master master.local/" -i /etc/hosts
 sudo sed -e '/^.*ubuntu-xenial.*/d' -i /etc/hosts
 
-sudo kubeadm init --api-advertise-addresses=${ADDRESS} --token=b9e6bb.6746bcc9f8ef8267
-#sudo kubeadm init --skip-preflight-checks --api-advertise-addresses=${ADDRESS} --token=b9e6bb.6746bcc9f8ef8267
+sudo sed -i -e 's/AUTHZ_ARGS=.*/AUTHZ_ARGS="/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+sudo systemctl daemon-reload
+
+sudo kubeadm init --use-kubernetes-version=v1.5.6 --api-advertise-addresses=${ADDRESS} --token=b9e6bb.6746bcc9f8ef8267
 sleep 15
 sudo kubectl apply -f /vagrant/romana-kubeadm-vagrant.yml
+
 
 # Add storage on master and export via NFS
 apt-get install nfs-kernel-server nfs-common -y
--- kubeadm-node-ubuntu.sh
+++ kubeadm-node-ubuntu.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
-set -x
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-get install curl \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo apt-key adv \
-#               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-#               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-#echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
+echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
 echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-#sudo apt-get install docker-engine=1.12.6-0~ubuntu-xenial -y
-sudo apt-get install docker-engine -y
-sudo apt-get install kubeadm kubectl kubernetes-cni -y
+sudo apt-get install docker-engine=1.12.* -y
 sudo service docker start
 sudo groupadd docker
-sudo usermod -aG docker ubuntu 
+sudo usermod -aG docker ubuntu
 sudo apt-get install python-pip -y
 sudo apt-get install joe -y
 sudo pip install --upgrade pip
 
+exit
 #Setup Kubernetes
+sudo apt-get install kubeadm=1.5.6* kubelet=1.5.6* kubectl=1.5.6* kubernetes-cni=0.5.1* -y
 ADDRESS="$(ip -4 addr show enp0s8 | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
 HOSTNAME=`hostname`
 sudo sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
 sudo sed -e '/^.*ubuntu-xenial.*/d' -i /etc/hosts
 
+sudo sed -i -e 's/AUTHZ_ARGS=.*/AUTHZ_ARGS="/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+sudo systemctl daemon-reload
 
 ## ## Update routing for Romana
 ## #sudo cat >> /etc/network/interfaces <<EOF
@@ -40,7 +38,6 @@ sudo sed -e '/^.*ubuntu-xenial.*/d' -i /etc/hosts
 ## #EOF
 ## #sudo ip route add 10.96.0.0/12 via 192.168.56.10
 
-#sudo kubeadm join --skip-preflight-checks --token=b9e6bb.6746bcc9f8ef8267 192.168.56.10
 sudo kubeadm join --token=b9e6bb.6746bcc9f8ef8267 192.168.56.10
 
 apt-get install nfs-common -y
