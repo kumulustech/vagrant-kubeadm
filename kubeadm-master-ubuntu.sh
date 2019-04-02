6f302ae67f9ebe1da10669d60d2791626a20916d
Robert Starmer <rstarmer@gmail.com>
2019-04-02 04:47:06 -0700

    Update CNI to use Calico

--- kubeadm-master-ubuntu.sh
+++ kubeadm-master-ubuntu.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates -y
-sudo apt-get install curl \
-    linux-image-extra-$(uname -r) \
-    linux-image-extra-virtual -y
+# Install Docker CE
+## Set up the repository:
+### Install packages to allow apt to use a repository over HTTPS
+sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+sudo apt-get update && sudo apt-get install docker-ce=18.06.2~ce~3-0~ubuntu -y
+echo '{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}' | sudo tee -a /etc/docker/daemon.json
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-key adv \
                --keyserver hkp://ha.pool.sks-keyservers.net:80 \
                --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
-echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install docker-engine -y
-sudo service docker start
 sudo groupadd docker
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker vagrant
 sudo apt-get install python-pip -y
 sudo apt-get install joe -y
 sudo -H pip install --upgrade pip
 
 #Setup Kubernetes
-sudo apt-get install kubeadm kubelet kubectl kubernetes-cni -y
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 ADDRESS="$(ip -4 addr show enp0s8 | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
 sudo sed -e "s/^.*master.*/${ADDRESS} master master.local/" -i /etc/hosts
 sudo sed -e '/^.*ubuntu-xenial.*/d' -i /etc/hosts
@@ -31,14 +48,17 @@ sudo sed -i -e 's/AUTHZ_ARGS=.*/AUTHZ_ARGS="/' /etc/systemd/system/kubelet.servi
 
 sudo systemctl daemon-reload
 
-sudo kubeadm init --apiserver-advertise-address=${ADDRESS} --token=b9e6bb.6746bcc9f8ef8267
+sudo kubeadm init --apiserver-advertise-address=${ADDRESS} --token=b9e6bb.6746bcc9f8ef8267 --pod-network-cidr=172.16.0.0/16
 sleep 15
 sudo mkdir -p /root/.kube/
 sudo cp /etc/kubernetes/admin.conf /root/.kube/config
-mkdir -p ~/.kube
-sudo cp /etc/kubernetes/admin.conf ~/.kube/config
+sudo mkdir -p ~vagrant/.kube
+sudo cp /etc/kubernetes/admin.conf ~vagrant/.kube/config
+sudo chown -R vagrant:vagrant ~vagrant/.kube
 sudo cp /etc/kubernetes/admin.conf /vagrant/
 
+sudo kubectl apply -f \
+https://docs.projectcalico.org/v3.6/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 #sudo kubectl apply -f /vagrant/romana-kubeadm-vagrant.yml
 
 
--- kubeadm-node-ubuntu.sh
+++ kubeadm-node-ubuntu.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates -y
-sudo apt-get install curl \
-    linux-image-extra-$(uname -r) \
-    linux-image-extra-virtual -y
+# Install Docker CE
+## Set up the repository:
+### Install packages to allow apt to use a repository over HTTPS
+sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+sudo apt-get update && sudo apt-get install docker-ce=18.06.2~ce~3-0~ubuntu -y
+echo '{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}' | sudo tee -a /etc/docker/daemon.json
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-key adv \
                --keyserver hkp://ha.pool.sks-keyservers.net:80 \
                --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
-echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install docker-engine -y
-sudo service docker start
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 sudo apt-get install python-pip -y
@@ -21,7 +33,12 @@ sudo apt-get install joe -y
 sudo pip install --upgrade pip
 
 #Setup Kubernetes
-sudo apt-get install kubeadm kubelet kubectl kubernetes-cni -y
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 ADDRESS="$(ip -4 addr show enp0s8 | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
 HOSTNAME=`hostname`
 sudo sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
@@ -38,6 +55,6 @@ sudo systemctl daemon-reload
 ## #sudo ip route add 10.96.0.0/12 via 192.168.56.10
 sudo mkdir /root/.kube/
 sudo cp /vagrant/admin.conf /root/.kube/config
-sudo kubeadm join --token=b9e6bb.6746bcc9f8ef8267 192.168.56.10:6443 --discovery-token-unsafe-skip-ca-verification
+sudo kubeadm join --token=b9e6bb.6746bcc9f8ef8267 192.168.56.10:6443 --discovery-token-unsafe-skip-ca-verification --pod-network-cidr=172.16.0.0/16
 
 apt-get install nfs-common -y
